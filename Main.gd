1d1767598b7dfc168df581c87c4c645ebcc2ed2c
fgonzale99 <fgonzale99@gmail.com>
2022-02-10 08:05:06 -0500

    Codigo ajustado con cuadrados

--- Main.gd
+++ Main.gd
@@ -1,16 +1,30 @@
 extends Node2D
 var Enemy = preload ("res://enemigo.tscn")
 
-const images = [preload("res://assets/enemies/box 1.png"),
-				preload("res://assets/enemies/computer 2.png"),
-				preload("res://assets/enemies/screen 3.png")]
+export (String) var selectedColor = "ffffff"
 
 
+
+
+
+#establece centrado de pantalla
 func _ready():
 	OS.set_window_position(
 		OS.get_screen_position(OS.get_current_screen()) + 
 		OS.get_screen_size()*0.5 - OS.get_window_size()*0.5)
+	spawn_Enemy()
+	
 
+
+func color_wheel_selection(coordenadasMouse):
+	var image=get_node("ColorWheel").get_texture().get_data()
+	image.lock()
+	var posicionAbsolutaWheel=get_node("ColorWheel").position
+	var pickedColor=image.get_pixel(coordenadasMouse.x-posicionAbsolutaWheel.x,
+	coordenadasMouse.y-posicionAbsolutaWheel.y)
+	image.unlock()	
+	return pickedColor
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -19,29 +33,17 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.pressed:
-			var image=get_node("ColorWheel").get_texture().get_data()
-			image.lock()
-			
-			var posicionAbsolutaWheel=get_node("ColorWheel").position
-			print(posicionAbsolutaWheel)
-			
-			
-			var pickedColor=image.get_pixel(event.position.x-posicionAbsolutaWheel.x,
-			event.position.y-posicionAbsolutaWheel.y)
-			get_node("CanvasLayer/Label").text=pickedColor.to_html(false)
-			get_node("CanvasLayer/Label").set("custom_colors/font_color", pickedColor)
-			image.unlock()
-			
+		if event.pressed:	
+			var pickedColor=color_wheel_selection(event.position)
+			selectedColor=pickedColor.to_html(false)
+			#get_node("CanvasLayer/Label").text=pickedColor.to_html(false)
+			#get_node("CanvasLayer/Label").set("custom_colors/font_color", pickedColor)
 			get_node("Graviton").modulate=Color(pickedColor)
 
 
 
-func _on_EnemyTimer_timeout():
-	
-		
+func spawn_Enemy():
 	randomize()
-	
 	var Enemy = load ("res://enemigo.tscn")
 	var e = Enemy.instance()
 	#e.rotation=rand_range(0,360)
@@ -50,5 +52,14 @@ func _on_EnemyTimer_timeout():
 	e.position.x=1250
 	e.get_node('Sprite').set_texture(preload('res://assets/triangle.png'))
 	add_child(e)
+
+
+
+func _on_EnemyTimer_timeout():
+	
+	spawn_Enemy()
+	
+	
+	
 	
 	
--- enemigo.gd
+++ enemigo.gd
@@ -3,23 +3,27 @@ extends KinematicBody2D
 var h_speed = 200
 var v_speed = 0
 
-export (String, FILE) var file_name = ""
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const images = [preload("res://assets/triangle.png"),
+				preload("res://assets/cuadrado.png")]
 
+const figuras=['Triangulo','Cuadrado']
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
+	var indiceImagen=int(rand_range(0,2))
+	
+	
+	get_node("Sprite").texture=images[indiceImagen]
+	
+	
+	
 	var newColor=Color(rand_range(0,1), rand_range(0,1),rand_range(0,1),1)
 	#var newColor=Color(0, 0, 1 ,1)
 	get_node("Sprite").modulate  = newColor
-	
-	
-
-
-	
 	pass # Replace with function body.
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,25 +32,14 @@ func _ready():
 func _physics_process(delta):
 	
 	var spriteColor= get_node("Sprite").modulate  
-	var selectedColor= Color(get_parent().get_node('CanvasLayer/Label').text)
-	
-	var li_c = selectedColor.linear_interpolate(spriteColor, 0.5) # Equivalent to Color(0.5, 0.5, 0.0)	
-	
+	var selectedColor= Color(get_parent().selectedColor)
 	var grav = sqrt(pow(selectedColor[0]-spriteColor[0],2) + pow(selectedColor[1]-spriteColor[1],2) + pow(selectedColor[2]-spriteColor[2],2))
 	print(grav)
 	var acc = 2.2
 	v_speed = v_speed + (sqrt(3) * acc/2 - grav * acc)
-
 	move_and_slide(Vector2(-h_speed, v_speed))
 	
-	
-	
-	
-	
-	
-	
-	
-	
+
 
 
 func _on_VisibilityNotifier2D_screen_exited():
